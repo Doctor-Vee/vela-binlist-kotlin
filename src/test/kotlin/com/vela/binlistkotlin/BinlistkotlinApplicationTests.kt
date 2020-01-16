7dde76b81acf924c9c54b42bb3eeb28a83838256
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2020-01-16 11:38:12 +0100

    Initial Commit

---
+++ src/test/kotlin/com/vela/binlistkotlin/BinlistkotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vela.binlistkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BinlistkotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
