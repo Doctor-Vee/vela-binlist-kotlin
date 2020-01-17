50275bef8b4a20ebf717d1decdb04f9ce7075a75
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2020-01-17 11:47:39 +0100

    Create card verification model

--- src/main/kotlin/com/vela/binlistkotlin/model/CardVerificationRecord.kt
+++ src/main/kotlin/com/vela/binlistkotlin/model/CardVerificationRecord.kt
@@ -4,22 +4,17 @@ import lombok.Data
 import java.util.*
 import javax.persistence.*
 
-@Data
-@Entity(name = "card_verification_records")
+@Entity
+@Table(name = "card_verification_records")
 class CardVerificationRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
-
-    @Column(name = "card_number")
-    var cardNumber: String? = null
+    var id: Long = 0
+    @Column(name = "card_number", nullable = false)
+    lateinit var cardNumber: String
 
     @Column(name = "request_date", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     var requestDate: Date? = null
-
-    constructor(cardNumber: String){
-        this.cardNumber = cardNumber
-    }
 
 
 }
